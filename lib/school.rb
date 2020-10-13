5ff5f9238c460d72b28c0ffe83a7c2e662b135e9
bclarke329 <bmendoza329@hotmail.com>
2020-10-12 21:16:12 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end 
+
+   def roster
+     @roster
+   end 
+
+   def add_student(student_name, grade)
+    
+        if @roster.include?(grade)
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+    end 
+    end 
+   
+    def grade(grade) 
+     @roster.fetch(grade)
+    end 
+
+    def sort
+        @roster.each do |grade, name|
+            name.sort!
+        end 
+    end 
+
+end 
